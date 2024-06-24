cfbcc917de1608d19dedfc846b61b0b348dee5f8
brigeBook <k9mm10feet@yahoo.co.jp>
2024-06-24 20:00:31 +0900

    silhouette

--- src/App.js
+++ src/App.js
@@ -1,24 +1,141 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
+import SilhouetteImage from "./silhouette";
+import { useEffect } from "react";
+import { getValue } from "@testing-library/user-event/dist/utils";
+
+// pokeAPIを使ってポケモンを当てるクイズアプリを作りたい
+// １　pokeAPIからデータを取得する。✅
+// ２　取得したデータからランダムに一つのポケモンを指定する ✅
 
 function App() {
+  // ポケAPI1
+  const [pokemonDetailsJp, setPokemonDetailsJp] = useState(undefined);
+  // ポケAPI2
+  const [pokemonDetails, setPokemonDetails] = useState(undefined);
+
+  const [loading, setLoding] = useState(false);
+
+  const [start, setStart] = useState(true);
+  //   答え
+  const [answerWord, setAnswerWord] = useState("");
+
+  let min = 1;
+  let max = 1000;
+
+  const QuizStrat = () => {
+    setLoding(true);
+
+    const randomNum = Math.floor(Math.random() * (max + 1 - min)) + min;
+
+    const pokemonDataJp =
+      "https://pokeapi.co/api/v2/pokemon-species/" + randomNum;
+    const pokemonData = "https://pokeapi.co/api/v2/pokemon/" + randomNum;
+
+    // APIを叩いてポケモンのJSONデータを取得する。
+    async function fetchData1() {
+      const data = await fetch(pokemonDataJp);
+      const res = await data.json();
+      setPokemonDetailsJp(res);
+    }
+    async function fetchData2() {
+      const data = await fetch(pokemonData);
+      const res = await data.json();
+      setPokemonDetails(res);
+      setLoding(false);
+    }
+    async function fetchData3() {
+      const data = await fetch(pokemonData);
+      const res = await data.json();
+      setPokemonDetails(res);
+      setLoding(false);
+    }
+    fetchData1();
+    fetchData2();
+  };
+
+  //   GET https://pokeapi.co/api/v2/pokemon-species/{id or name}/
+
+  const Start = () => {
+    if (loading) {
+      setStart(false);
+      return <div>ロード中.....</div>;
+    }
+  };
+
+  const StartButton = () => {
+    if (!start) return;
+    return (
+      <button
+        onClick={QuizStrat}
+        className="StartButton bg-red-500 hover:bg-red-400 text-white rounded px-4 py-2"
+      >
+        クイズを始める
+      </button>
+    );
+  };
+
+  const Siruetto = () => {
+    if (!pokemonDetails) return;
+    return (
+      <>
+        <div className="flex text-center">
+          <SilhouetteImage
+            width={300}
+            height={300}
+            imageURL={
+              pokemonDetails.sprites.other["official-artwork"].front_default
+            }
+          />
+        </div>
+      </>
+    );
+  };
+
+  console.log(pokemonDetails);
+  console.log(pokemonDetailsJp);
+
+  useEffect(() => {}, {});
+
+  console.log(answerWord);
+
+  // 画像
+  // pokemonDetails.sprites.other.official-artwork.front_default
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* 外枠 */}
+      <div className="flex flex-col text-center mt-28">
+        <div className="mx-auto my-auto">
+          <div
+            hidden={pokemonDetails ? true : false}
+            className="font-bold text-2xl mt-40 mb-5"
+          >
+            ポケモンクイズ
+          </div>
+          <Start />
+          <StartButton />
+        </div>
+        <div className="flex">
+          <div className="mx-auto">
+            <Siruetto />
+            <input
+              onChange={(event) => setAnswerWord(event.target.value)}
+              onBlur={(event) => setAnswerWord(event.target.value)}
+              hidden={pokemonDetails ? false : true}
+              className="shadow appearance-none border rounded w-full mt-6 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </div>
+        </div>
+        <div>
+          <button
+            hidden={pokemonDetails ? false : true}
+            className="bg-blue-500 hover:bg-blue-400 text-white rounded px-4 py-2 mt-6"
+          >
+            答える
+          </button>
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/silhouette.jsx
@@ -0,0 +1,71 @@
+import React, { useRef, useEffect } from "react";
+
+const SilhouetteImage = ({ width, height, imageURL }) => {
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    if (!imageURL) {
+      console.error("Image URL is not provided");
+      return;
+    }
+
+    const loadImage = async () => {
+      const canvas = canvasRef.current;
+      const context = canvas.getContext("2d");
+
+      const image = new Image();
+      image.crossOrigin = "Anonymous";
+      image.src = imageURL;
+
+      await new Promise((resolve) => {
+        image.onload = () => {
+          resolve();
+        };
+      });
+
+      canvas.width = width;
+      canvas.height = height;
+
+      // 画像をキャンバスに描画
+      context.drawImage(image, 0, 0, width, height);
+
+      // キャンバスのピクセルデータを取得
+      const imageData = context.getImageData(0, 0, width, height);
+      const data = imageData.data;
+
+      // 背景の色を取得（左上のピクセルの色を背景色とする）
+      const backgroundColor = {
+        red: data[0],
+        green: data[1],
+        blue: data[2],
+      };
+
+      // シルエット化と黒で塗りつぶし
+      for (let i = 0; i < data.length; i += 4) {
+        // 背景色と同じ場合は白で塗りつぶす
+        if (
+          data[i] === backgroundColor.red &&
+          data[i + 1] === backgroundColor.green &&
+          data[i + 2] === backgroundColor.blue
+        ) {
+          data[i] = 255; // 赤
+          data[i + 1] = 255; // 緑
+          data[i + 2] = 255; // 青
+        } else {
+          data[i] = 0; // 赤
+          data[i + 1] = 0; // 緑
+          data[i + 2] = 0; // 青
+        }
+      }
+
+      // シルエット画像をキャンバスに再描画
+      context.putImageData(imageData, 0, 0);
+    };
+
+    loadImage();
+  }, [width, height, imageURL]);
+
+  return <canvas ref={canvasRef} />;
+};
+
+export default SilhouetteImage;
